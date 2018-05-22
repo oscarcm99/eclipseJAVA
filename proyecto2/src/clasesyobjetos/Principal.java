9338bdd1fa46ef61f90ad1307c2d7045cc9347c6
oscar calderon <gorri0302@gmail.com>
2018-05-22 12:51:47 +0200

    Java

---
+++ proyecto2/src/clasesyobjetos/Principal.java
@@ -0,0 +1,11 @@
+package clasesyobjetos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		coche c1 = new coche("1234JVL","seat", "toledo", 2003, "negro", 24321.12, 'D', true);
+		System.out.println(c1.toString());
+	}
+
+}
---
+++ proyecto2/src/clasesyobjetos/coche.java
@@ -0,0 +1,101 @@
+package clasesyobjetos;
+
+public class coche {
+	// 1. Definir los atributos
+	String matricula;
+	String marca;
+	String modelo;
+	int año;
+	String color;
+	double precio;
+	char motor; // D Diesel G Gasoil H Hibrido
+	boolean itv;// True La ha pasado false no la pasa
+	// 2. Implementar metodo constructor publico y con mismo nombre
+
+	public coche(String matricula, String marca, String modelo, int año, String color, double precio, char motor, 
+			boolean itv) {
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.año = año;
+		this.color = color;
+		this.precio = precio;
+		this.motor = motor;
+		this.itv = itv;
+	}
+	// 3. Metodos getters and setters get me sirve para obtener valor de cada atributo set para cambiarlo
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public char getMotor() {
+		return motor;
+	}
+
+	public void setMotor(char motor) {
+		this.motor = motor;
+	}
+
+	public boolean isItv() {
+		return itv;
+	}
+
+	public void setItv(boolean itv) {
+		this.itv = itv;
+	}
+
+	@Override
+	// 4. Metodo to string
+	public String toString() {
+		return "coche [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", año=" + año + ", color="
+				+ color + ", precio=" + precio + ", motor=" + motor + ", itv=" + itv + "]";
+	}
+	
+
+	
+}
