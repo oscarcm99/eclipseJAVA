ff917db53841654b35b0cb15afb7bc44b7a842ef
oscar calderon <gorri0302@gmail.com>
2018-05-29 12:59:08 +0200

    clases

---
+++ Proyecto4/src/Alumno.java
@@ -0,0 +1,10 @@
+
+public class Alumno extends Persona {
+	boolean becado;
+	public Alumno(String nombre, String apellidos, int edad, char sexo) {
+		super(nombre, apellidos, edad, sexo);
+		// TODO Auto-generated constructor stub
+		this.becado = new ;
+	}
+
+}
